5add3c16cb65e0326c41f340f2dc8f5719047266
sanin <saninthottungalhere@gmail.com>
2026-05-15 18:39:03 +0530

    feat: bottom nav icons added

--- lib/app/features/admin/presentation/bottom_nav/bottom_nav_page.dart
+++ lib/app/features/admin/presentation/bottom_nav/bottom_nav_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 class AdminBottomNav extends StatelessWidget {
   const AdminBottomNav({
@@ -13,6 +14,26 @@ class AdminBottomNav extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: navigationShell,
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: navigationShell.currentIndex,
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(LucideIcons.house),
+            label: "Home",
+          ),
+
+          NavigationDestination(
+            icon: Icon(LucideIcons.vegan),
+            label: "Vegetables",
+          ),
+
+          NavigationDestination(
+            icon: Icon(LucideIcons.shoppingBag),
+            label: "Orders",
+          ),
+        ],
+        onDestinationSelected: navigationShell.goBranch,
+      ),
     );
   }
 }
